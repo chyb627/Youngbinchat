a960d4dd5ee4de0566a4e513869bb14de5525286
cha <chyb627@naver.com>
2023-02-28 17:01:52 +0900

    메시지 불러오기 기능

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -90,10 +90,11 @@ const ChatScreen = () => {
   const { params } = useRoute<RouteProp<RootStackParamList, 'Chat'>>();
   const { other, userIds } = params;
 
-  const { loadingChat, chat, sendMessage } = useChat(userIds);
+  const { loadingChat, chat, sendMessage, messages, loadingMessages } = useChat(userIds);
   const [text, setText] = useState('');
   const sendDisabled = useMemo(() => text.length === 0, [text]);
   const { user: me } = useContext(AuthContext);
+  const loading = loadingChat || loadingMessages;
 
   const onChangeText = useCallback((newText: string) => {
     setText(newText);
@@ -126,7 +127,19 @@ const ChatScreen = () => {
           />
         </View>
 
-        <View style={styles.messageList} />
+        <FlatList
+          style={styles.messageList}
+          data={messages}
+          renderItem={({ item: message }) => {
+            return (
+              <View>
+                <Text>{message.user.name}</Text>
+                <Text>{message.text}</Text>
+                <Text>{message.createdAt.toISOString()}</Text>
+              </View>
+            );
+          }}
+        />
 
         <View style={styles.inputContainer}>
           <View style={styles.textInputContainer}>
@@ -147,12 +160,12 @@ const ChatScreen = () => {
         </View>
       </View>
     );
-  }, [chat, onChangeText, onPressSendButton, sendDisabled, text]);
+  }, [chat, messages, onChangeText, onPressSendButton, sendDisabled, text]);
 
   return (
     <Screen title={other.name}>
       <View style={styles.container}>
-        {loadingChat ? (
+        {loading ? (
           <View style={styles.loadingContainer}>
             <ActivityIndicator />
           </View>
